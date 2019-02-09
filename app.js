b36c5d02b32cb7a841fa2c3ff85c5c10f9206904
thallmen <camhallmen@gmail.com>
2019-02-08 23:22:14 -0500

    added second track functionality and cosmetic upgrades

--- app.js
+++ app.js
@@ -17,26 +17,33 @@ var drumSurfer = WaveSurfer.create({
     progressColor: 'green',
 });
 
-// var leadSurferTwo = WaveSurfer.create({
-//     container: '#lead-form2',
-//     waveColor: 'blue',
-//     progressColor: 'green',
-// });
+var leadSurferTwo = WaveSurfer.create({
+    container: '#lead-form2',
+    waveColor: 'blue',
+    progressColor: 'green',
+});
 
-// var rhythmSurferTwo = WaveSurfer.create({
-//     container: '#rhythm-form2',
-//     waveColor: 'blue',
-//     progressColor: 'green',
-// });
+var rhythmSurferTwo = WaveSurfer.create({
+    container: '#rhythm-form2',
+    waveColor: 'blue',
+    progressColor: 'green',
+});
 
-// var drumSurferTwo = WaveSurfer.create({
-//     container: '#drum-form2',
-//     waveColor: 'blue',
-//     progressColor: 'green',
-// });
+var drumSurferTwo = WaveSurfer.create({
+    container: '#drum-form2',
+    waveColor: 'blue',
+    progressColor: 'green',
+});
 
 $("#poppunklead1").on("click", function() {
-    leadSurfer.load("https://firebasestorage.googleapis.com/v0/b/mf-test-bafa6.appspot.com/o/poppunklead1.mp3?alt=media&token=385fd824-7b23-4651-8d15-71ce0755480f")
+    var choice = prompt("Section 1 or 2?")
+    if (choice === "1") {
+        leadSurfer.load("https://firebasestorage.googleapis.com/v0/b/mf-test-bafa6.appspot.com/o/poppunklead1.mp3?alt=media&token=385fd824-7b23-4651-8d15-71ce0755480f")
+    } else if (choice === "2") {
+        leadSurferTwo.load("https://firebasestorage.googleapis.com/v0/b/mf-test-bafa6.appspot.com/o/poppunklead1.mp3?alt=media&token=385fd824-7b23-4651-8d15-71ce0755480f")
+    } else {
+        alert("Please select a valid section!")
+    }
 })
 
 $("#poppunklead2").on("click", function() {
@@ -44,7 +51,14 @@ $("#poppunklead2").on("click", function() {
 })
 
 $("#poppunkrhythm1").on("click", function() {
-    rhythmSurfer.load("https://firebasestorage.googleapis.com/v0/b/mf-test-bafa6.appspot.com/o/poppunkrhythm1.mp3?alt=media&token=483800bd-3538-4288-be7b-0809da9548af")
+    var choice = prompt("Section 1 or 2?")
+    if (choice === "1") {
+        rhythmSurfer.load("https://firebasestorage.googleapis.com/v0/b/mf-test-bafa6.appspot.com/o/poppunkrhythm1.mp3?alt=media&token=483800bd-3538-4288-be7b-0809da9548af")
+    } else if (choice === "2") {
+        rhythmSurferTwo.load("https://firebasestorage.googleapis.com/v0/b/mf-test-bafa6.appspot.com/o/poppunkrhythm1.mp3?alt=media&token=483800bd-3538-4288-be7b-0809da9548af")
+    } else {
+        alert("Please select a valid section!")
+    }
 })
 
 $("#poppunkrhythm2").on("click", function() {
@@ -52,7 +66,14 @@ $("#poppunkrhythm2").on("click", function() {
 })
 
 $("#poppunkdrums1").on("click", function() {
-    drumSurfer.load("https://firebasestorage.googleapis.com/v0/b/mf-test-bafa6.appspot.com/o/poppunkdrums1.mp3?alt=media&token=0b1ed9a1-d29e-45aa-bb5b-7e05914b2a2c")
+    var choice = prompt("Section 1 or 2?")
+    if (choice === "1") {
+        drumSurfer.load("https://firebasestorage.googleapis.com/v0/b/mf-test-bafa6.appspot.com/o/poppunkdrums1.mp3?alt=media&token=0b1ed9a1-d29e-45aa-bb5b-7e05914b2a2c")
+    } else if (choice === "2") {
+        drumSurferTwo.load("https://firebasestorage.googleapis.com/v0/b/mf-test-bafa6.appspot.com/o/poppunkdrums1.mp3?alt=media&token=0b1ed9a1-d29e-45aa-bb5b-7e05914b2a2c")
+    } else {
+        alert("Please select a valid section!")
+    }
 })
 
 $("#poppunkdrums2").on("click", function() {
@@ -64,8 +85,23 @@ $("#poppunkdrums2").on("click", function() {
 // Play
 $("#play").on("click", function() {
     leadSurfer.playPause()
+    // leadSurferTwo.playPause()
     rhythmSurfer.playPause()
     drumSurfer.playPause()
+
+})
+
+// Play Section Two
+leadSurfer.on("finish", function() {
+    leadSurferTwo.play()
+})
+
+rhythmSurfer.on("finish", function() {
+    rhythmSurferTwo.play()
+})
+
+drumSurfer.on("finish", function() {
+    drumSurferTwo.play()
 })
 
 // Fast Forward
@@ -82,12 +118,12 @@ $("#rewind").on("click", function() {
     drumSurfer.skipBackward()
 })
 
-// Set track volume
+// Set song volume
 $("#master-volume").on("change", function() {
     var rangeValue = $("#master-volume").val()
     newVolume = rangeValue/100
     leadSurfer.setVolume(newVolume)
-    // leadSurferTwo.setVolume(newVolume)
+    leadSurferTwo.setVolume(newVolume)
     rhythmSurfer.setVolume(newVolume)
     // rhythmSurferTwo.setVolume(newVolume)
     drumSurfer.setVolume(newVolume)
@@ -99,7 +135,7 @@ $("#lead-volume").on("change", function() {
     var rangeValue = $("#lead-volume").val()
     newVolume = rangeValue/100
     leadSurfer.setVolume(newVolume)
-    // leadSurferTwo.setVolume(newVolume)
+    leadSurferTwo.setVolume(newVolume)
 })
 
 $("#rhythm-volume").on("change", function() {
@@ -114,6 +150,33 @@ $("#drums-volume").on("change", function() {
     newVolume = rangeValue/100
     drumSurfer.setVolume(newVolume)
     // leadSurferTwo.setVolume(newVolume)
+})
+
+// Mute individual tracks
+$("#mute-lead").on("click", function() {
+    leadSurfer.toggleMute()
+    leadSurferTwo.toggleMute()
+})
+
+$("#mute-rhythm").on("click", function() {
+    rhythmSurfer.toggleMute()
+})
+
+$("#mute-drums").on("click", function() {
+    drumSurfer.toggleMute()
+})
+
+// Delete individual tracks
+$("#delete-lead").on("click", function() {
+    leadSurfer.empty()
+})
+
+$("#delete-rhythm").on("click", function() {
+    rhythmSurfer.empty()
+})
+
+$("#delete-drums").on("click", function() {
+    drumSurfer.empty()
 })
 
 // Choose wave color
